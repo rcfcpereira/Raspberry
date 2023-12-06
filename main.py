de17b1c74f00aa6617a1592f8f850e02bcd1fbbb
rcfcpereira <1181915@ISEP.IPP.PT>
2023-12-06 23:37:18 +0000

    v 0.0
    Co-authored-by: rcfcpereira <94740055+rcfcpereira@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,99 @@
+import time,sys
+from RPi import GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.IN, GPIO.PUD_UP)
+
+
+if sys.platform == 'uwp':
+    import winrt_smbus as smbus
+    bus = smbus.SMBus(1)
+else:
+    import smbus
+    import RPi.GPIO as GPIO
+    rev = GPIO.RPI_REVISION
+    if rev == 2 or rev == 3:
+        bus = smbus.SMBus(1)
+    else:
+        bus = smbus.SMBus(0)
+
+# I2C addresses
+DISPLAY_TEXT_ADDR = 0x3e
+
+
+
+# send command to display (no need for external use)    
+def textCommand(cmd):
+    bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
+
+# set display text \n for second line(or auto wrap)     
+def setText(text):
+    textCommand(0x01) # clear display
+    time.sleep(.05)
+    textCommand(0x08 | 0x04) # display on, no cursor
+    textCommand(0x28) # 2 lines
+    time.sleep(.05)
+    count = 0
+    row = 0
+    for c in text:
+        if c == '\n' or count == 16:
+            count = 0
+            row += 1
+            if row == 2:
+                break
+            textCommand(0xc0)
+            if c == '\n':
+                continue
+        count += 1
+        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+
+#Update the display without erasing the display
+def setText_norefresh(text):
+    textCommand(0x02) # return home
+    time.sleep(.05)
+    textCommand(0x08 | 0x04) # display on, no cursor
+    textCommand(0x28) # 2 lines
+    time.sleep(.05)
+    count = 0
+    row = 0
+    while len(text) < 32: #clears the rest of the screen
+        text += ' '
+    for c in text:
+        if c == '\n' or count == 16:
+            count = 0
+            row += 1
+            if row == 2:
+                break
+            textCommand(0xc0)
+            if c == '\n':
+                continue
+        count += 1
+        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+
+# example code
+if __name__=="__main__":
+    clock = 0
+
+    setText("Drag Race")
+    time.sleep(2)
+
+    while GPIO.input(18) == 0:
+         setText("Robo Fora da linha")
+         time.sleep(5)
+
+    textCommand(0x01)
+
+    while (GPIO.input(18) == 1):
+        
+        setText_norefresh("Time\n {}".format(str(clock)))
+        clock = 1+clock
+        time.sleep(0.001)
+    
+    textCommand(0x01)
+    setText("Tempo final:\n {}".format(str(clock)))
+
+    time.sleep(10)
+
+    exit("End race!!!!!")
+
