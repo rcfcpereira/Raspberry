e886d67d9408d709f8fe2742e7e2640492119363
rcfcpereira <94740055+rcfcpereira@users.noreply.github.com>
2023-12-11 05:51:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ encoder = H264Encoder()
 
 #Define GPIOS
 
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(5, GPIO.IN)
 GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+#Led Star button Off
+GPIO.output(16,0)
 
+#########################################
+LCD 16x2
 # I2C addresses
 bus = smbus.SMBus(1)
 DISPLAY_TEXT_ADDR = 0x3e
-
-GPIO.output(16,0)
 
 # send command to display (no need for external use)    
 def textCommand(cmd):
@@ -70,24 +71,24 @@ def setText_norefresh(text):
         count += 1
         bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
 
-# example code
+#Coder
 if __name__=="__main__":
+    #Start clock
     clock = 0
-
-    
+#Start Message
     setText("Drag Race")
     time.sleep(2)
-
+#Wait for Race Car to stay on start mark 
     while (GPIO.input(5) == 0):
         
-        setText("Robo Fora da linha")
+        setText("Race Car:\nGo to Start Mark")
         time.sleep(0.5)
-
+#Start Race
     setText("Press the button to start race")
     GPIO.output(16,1)
 
     while True:
-        
+        # Press Button to Start Race
         if (GPIO.input(17) == 0):
             
             for x in range(6):
@@ -100,25 +101,32 @@ if __name__=="__main__":
 
     textCommand(0x01)
 
+    # Start Camera recording 
     picam2.start_recording(encoder, "race.h264")
+    
+    #Start Race
 
+    #Clock work waiting for Race Car pass finish sensor 
+    
     while (GPIO.input(18) == 1):
         
         setText_norefresh("Time\n {}".format(str(clock)))
         clock = 1+clock
         time.sleep(0.01)
-    
+
+    # End Race
     textCommand(0x01)
     setText("Tempo final:\n {}".format(str(clock)))
-    
+    # Stop recodind
     picam2.stop_recording()
     time.sleep(10)
 
-    setText("End race!!!!!")
-    
+    setText("End race!!!!!")    
     time.sleep(5)
+
+    #Clear Display
     textCommand(0x01)
-    
+    #Clear GPIO settings to default 
     GPIO.cleanup()
-    
+    #end 
     exit()
