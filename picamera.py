9f68a5ef454480f29d001c104143953fc23cdeef
Ruben Pereira <1181915@gmail.com>
2023-12-13 06:19:14 +0000

    . Video record near 200 fps

--- picamera.py
+++ picamera.py
@@ -2,8 +2,6 @@ from picamera2 import Picamera2, controls
 
 picam2 = Picamera2()
 
-picam2.configure(picam2.create_video_configuration(main = {"size" : (640,480)}))
-
-picam2.framerate = 120.0
+picam2.configure(picam2.create_video_configuration(main = {"size" : (640,480)}, controls  = {'FrameRate': 200}))
 
 picam2.start_and_record_video("test.mp4", duration= 60)
